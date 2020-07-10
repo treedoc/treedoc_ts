45ec1c0ee60dd6cd331f0058ecd08fc62aa0b3e5
Jianwu Chen <jianwche@jianwche-mn1.linkedin.biz>
2020-07-10 07:33:57 -0700

    v0.2.7

--- src/objcoder/TDObjectCoder.ts
+++ src/objcoder/TDObjectCoder.ts
@@ -86,7 +86,7 @@ export default class TDObjectCoder {
     } else {
       // Object or Map
       target.type = TDNodeType.MAP;
-      if (opt.showType && obj.constructor && obj.constructor.name != 'Object')
+      if (opt.showType && obj.constructor && obj.constructor.name !== 'Object')
         target.createChild(this.KEY_TYPE).setValue(obj.constructor.name)
       for (const k of Object.keys(obj))
         this.encode(obj[k], opt, target.createChild(k), ctx);
