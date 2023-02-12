39955fdd7eba440a7ee9420a3e43191ab1592b2d
jianwu chen <jianwu@gmail.com>
2023-02-12 11:29:44 -0800

    v0.3.33

---
+++ src/json/TDJSONOption.ts
@@ -0,0 +1,12 @@
+import { TDNodeType } from '../TDNode';
+
+export class TDJSONOption {
+  KEY_ID = `$id`;
+  KEY_TYPE = "$type";
+  deliminatorKey = ":";
+  deliminatorValue = ",";
+  deliminatorObjectStart = "{";
+  deliminatorObjectEnd = "}";
+  deliminatorArrayStart = "[";
+  deliminatorArrayEnd = "]";
+}
