a2a9364f19d2cc68329e4d27590e61cf332ef631
jianwu chen <jianwu@gmail.com>
2023-09-25 16:20:35 -0700

    .

--- src/json/TDJSONParserOption.ts
+++ src/json/TDJSONParserOption.ts
@@ -45,37 +45,4 @@ export class TDJSONParserOption extends TDJSONOption {
     this.deliminatorArrayEnd = end;
     return this;
   }
-
-  // Package scopes used by parser
-  _termValue = "";
-  _termValueInMap = "";
-  _termValueInArray = "";
-  _termKey = "";
-  /** Quote need if a string contains any chars */
-  _quoteNeededChars = "";
-  _termValueStrs: string[] = [];
-  _termKeyStrs: string[] = [];
-
-  buildTerms() {
-    this._termValue = "\n\r" + this.deliminatorKey + this.deliminatorObjectStart;  // support tree with a type in the form of "type{attr1:val1}", key1:key2:type{att1:val1}    
-    this._termKey = this.deliminatorObjectStart + this.deliminatorObjectEnd + this.deliminatorArrayStart;
-    this._termValueStrs = [];
-    this._termKeyStrs = [];
-    if (this.deliminatorValue.length === 1) {  // If more than 1, will use separate string collection as term
-      this._termValue += this.deliminatorValue;
-      this._termKey += this.deliminatorValue;
-    } else {
-      this._termValueStrs.push(this.deliminatorValue);
-      this._termKeyStrs.push(this.deliminatorValue);
-    }
-    if (this.deliminatorKey.length === 1)
-      this._termKey += this.deliminatorKey;
-    else
-      this._termKeyStrs.push(this.deliminatorKey);
-
-    this._termValueInMap = this._termValue + this.deliminatorObjectEnd + this.deliminatorArrayEnd; // It's possible object end is omitted for path compression. e.g [a:b:c]
-    this._termValueInMap = this._termValue + this.deliminatorObjectEnd;
-    this._termValueInArray = this._termValue + this.deliminatorArrayEnd;
-    this._quoteNeededChars = this._termValue + this.deliminatorObjectEnd + this.deliminatorArrayEnd + this.deliminatorKey + this.deliminatorValue + this.quoteChars;
-  }
 }
